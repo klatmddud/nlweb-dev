d1d50ac52c3e10a416a7d018ca93715f34b108f8
klatmddud <klatmddud@gmail.com>
2025-12-26 23:40:19 +0900

    add core/config/JacksonConfig.java

---
+++ src/main/java/com/nlweb/core/config/JacksonConfig.java
@@ -0,0 +1,28 @@
+package com.nlweb.core.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfig {
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+
+        return JsonMapper.builder()
+                .addModule(module)
+                .build();
+    }
+
+}
